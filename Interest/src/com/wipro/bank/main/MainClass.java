0bec3e18fd2b4b44f8e92b2585b6432f4d41d2ba
nalayini809 <nalayinipalanisamy79@gmail.com>
2025-09-18 14:03:49 +0530

    Update MainClass.java
    Update MainClass to allow 5 user input iterations

--- Interest/src/com/wipro/bank/main/MainClass.java
+++ Interest/src/com/wipro/bank/main/MainClass.java
@@ -6,22 +6,23 @@ import java.util.Scanner;
 public class MainClass {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter Tenure (in years): ");
-        int tenure = sc.nextInt();
-
-        System.out.print("Enter Principal: ");
-        float principal = sc.nextFloat();
-
-        System.out.print("Enter Age: ");
-        int age = sc.nextInt();
-
-        System.out.print("Enter Gender: ");
-        String gender = sc.next();
-
         BankService service = new BankService();
-        service.calculate(principal, tenure, age, gender);
+        for (int i=1;i<6;i++) {
+            System.out.println("\n Test Case " + i );
 
+            System.out.print("Enter Tenure (in years): ");
+            int tenure = sc.nextInt();
+
+            System.out.print("Enter Principal: ");
+            float principal = sc.nextFloat();
+
+            System.out.print("Enter Age: ");
+            int age = sc.nextInt();
+            
+            System.out.print("Enter Gender: ");
+            String gender = sc.next();
+            service.calculate(principal, tenure, age, gender);
+        }
         sc.close();
     }
 }
